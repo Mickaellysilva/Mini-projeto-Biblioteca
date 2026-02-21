dab5996be3c6cd98d30603ba68da4e5c03aa1f9d
me.mickaelly <104102435+Mickaellysilva@users.noreply.github.com>
2026-02-21 10:24:57 -0300

    Add abstract Usuario class with basic properties

---
+++ model/Usuario.java
@@ -0,0 +1,16 @@
+package model;
+
+public abstract class Usuario {
+    private String nome;
+    private String email;
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public abstract void exibirInfo();
+}
