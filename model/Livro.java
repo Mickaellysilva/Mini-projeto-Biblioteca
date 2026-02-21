10128a437725d8c616a2b5b5d4d601229facd705
me.mickaelly <104102435+Mickaellysilva@users.noreply.github.com>
2026-02-21 10:24:20 -0300

    Add Livro class for book management

---
+++ model/Livro.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Livro: %s | Autor: %s | Status: %s",
+                titulo, autor, (disponivel ? "Disponível" : "Emprestado"));
+    }
+}
